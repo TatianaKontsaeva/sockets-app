45f914cd9ddbac7786b87c7522b86a64f372698f
Tatiana <tanya_13.88@mail.ru>
2023-06-21 12:44:40 +0300

    rooms and midleware added

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:8080",
+    origin: "http://localhost:8081",
   },
 });
 
@@ -21,9 +21,23 @@ app.listen(3000, async () => {
 });
 
 //Запуск сокет-сервера
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+  if (token === "secret") {
+    next();
+  } else {
+    console.log(socket, "not auth");
+    socket.emit("connection_error", {
+      message: "not auth",
+    });
+  }
+});
+
 io.on("connection", (socket) => {
-  socket.emit("connected", {
-    message: "connect successful",
+  socket.on("message", (data) => {
+    socket.join("room:" + data.room_id);
+    io.to("room:" + data.room_id).emit("message", data.message);
+    socket.emit("message", data);
   });
 
   socket.on("message", (arg) => {
@@ -31,7 +45,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", (reason) => {
-    console.log('socket out', reason);
+    console.log("socket out", reason);
   });
 });
 
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,35 +1,54 @@
 <template>
   <q-page class="flex-center column">
-    <q-input 
-      outlined 
-      class="q-pa-md"
-      v-model="message"
-    />
-    <q-btn label="Send" @click="send"/>
+    <q-input outlined class="q-pa-md input" v-model="room_id" />
+    <q-input outlined class="q-pa-md input" v-model="message" />
+    <q-btn label="Send" @click="send" />
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 const { io } = require("socket.io-client");
-const socket = io("http://localhost:3001");
-
-socket.on("connected", (arg) => {
-  console.log(arg); // world
+const socket = io("http://localhost:3001", {
+  auth: {
+    token: 'secret'
+  }
 });
+
+// socket.on("connected", (arg) => {
+//   console.log(arg); // world
+// });
+
+socket.on("message", (data) => {
+  console.log(data); 
+});
+
+socket.on("connection_error", (data) => {
+  console.log(data); 
+});
+
 
 export default defineComponent({
   name: "IndexPage",
 
   data() {
     return {
-      message: null
-    }
+      message: null,
+      room_id: 1,
+    };
   },
   methods: {
-    send () {
-      socket.emit("message", this.message)
-    }
-  }
+    send() {
+      socket.emit("message", {
+        message: this.message,
+        room_id: this.room_id,
+      });
+    },
+  },
 });
 </script>
+<style>
+.input {
+  width: 50%;
+}
+</style>
